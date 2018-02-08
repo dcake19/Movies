6d8173dfdf509f5e15298d68aaf2edff0b293af9
dcake19 <dcakebread82@gmail.com>
2018-02-08 17:44:53 +0000

    link people to movies

--- app/src/main/java/com/example/android/movies/ui/people/detailed/credits/PeopleCreditsPresenter.kt
+++ app/src/main/java/com/example/android/movies/ui/people/detailed/credits/PeopleCreditsPresenter.kt
@@ -2,13 +2,11 @@ package com.example.android.movies.ui.people.detailed.credits
 
 import com.example.android.movies.BuildConfig
 import com.example.android.movies.RxSchedulerProvider
+import com.example.android.movies.api.data.people.Cast
 import com.example.android.movies.api.data.people.PersonCredits
-import com.example.android.movies.ui.movies.detailed.credits.MovieCreditsContract
-import com.example.android.movies.ui.people.detailed.credits.PeopleCreditsContract
+import com.example.android.movies.util.TextUtil
 import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
@@ -17,7 +15,9 @@ class PeopleCreditsPresenter @Inject constructor(val interactor:PeopleCreditsInt
     : PeopleCreditsContract.Presenter {
 
     lateinit var view: PeopleCreditsContract.View
-    private lateinit var personCredits:PersonCredits
+   // private lateinit var personCredits:PersonCredits
+    private lateinit var cast: List<Cast>
+    private lateinit var crew: List<CrewMultipleJobs>
     private var downloadComplete = false
 
 
@@ -27,15 +27,20 @@ class PeopleCreditsPresenter @Inject constructor(val interactor:PeopleCreditsInt
 
     override fun downloadCredits(id: Int) {
         if (downloadComplete)
-            view.display( personCredits.cast.size, personCredits.crew.size)
+            view.display(cast.size,crew.size)
+          //  view.display( personCredits.cast.size, personCredits.crew.size)
         else {
             val observable = interactor.getPersonCredits(id.toString(), BuildConfig.TMDB_API_KEY)
             observable.subscribeOn(rxSchedulerProvider.subscribeOn())
                     .observeOn(rxSchedulerProvider.observeOn())
-                    .subscribe(object : Observer<PersonCredits> {
-                        override fun onNext(t: PersonCredits) {
-                            personCredits = t
-                            view.display(personCredits.cast.size,personCredits.crew.size)
+                    .map { it -> Pair(it.cast,setCrewWithMultipleJobs(it)) }
+                    .subscribe(object : Observer<Pair<List<Cast>,List<CrewMultipleJobs>>> {
+                        override fun onNext(t: Pair<List<Cast>,List<CrewMultipleJobs>>) {
+                            //personCredits = t
+                            cast = t.first
+                            crew = t.second
+                            //view.display(personCredits.cast.size,personCredits.crew.size)
+                            view.display(cast.size,crew.size)
                         }
 
                         override fun onError(e: Throwable?) {
@@ -53,43 +58,109 @@ class PeopleCreditsPresenter @Inject constructor(val interactor:PeopleCreditsInt
         }
     }
 
+    private fun setCrewWithMultipleJobs(personCredits: PersonCredits): List<CrewMultipleJobs>{
+        val map = HashMap<Int,CrewMultipleJobs>()
+        var order = 0
+        for(c in personCredits.crew){
+            if(map.containsKey(c.id))
+                map[c.id]!!.jobs.add(c.job)
+            else {
+                map.put(c.id, CrewMultipleJobs(c.id, order, c.title,
+                        arrayListOf(c.job), c.posterPath ?: "",
+                        c.backdropPath?:""))
+                order++
+            }
+        }
+
+        val crewList = map.toList().map {it->it.second}
+        return crewList.sortedBy {it.order }
+    }
+
+//    override fun getCastMovieId(position: Int): Int {
+//        return personCredits.cast.get(position).id
+//    }
+//
+//    override fun getCastMovieName(position: Int): String {
+//        return personCredits.cast.get(position).title
+//    }
+//
+//    override fun getCharacter(position: Int): String {
+//        return personCredits.cast.get(position).character
+//    }
+//
+//    override fun getCastMoviePosterPath(position: Int): String {
+//        return personCredits.cast.get(position).posterPath?:""
+//    }
+//
+//    override fun getCrewMovieId(position: Int): Int {
+//        return personCredits.crew.get(position).id
+//    }
+//
+//    override fun getCrewMovieName(position: Int): String {
+//        return personCredits.crew.get(position).title
+//    }
+//
+//    override fun getJob(position: Int): String {
+//        return personCredits.crew.get(position).job
+//    }
+//
+//    override fun getCrewMoviePosterPath(position: Int): String {
+//        return personCredits.crew.get(position).posterPath?:""
+//    }
+//
+//    override fun getCastMovieBackdropPath(position: Int): String {
+//        return personCredits.cast[position].backdropPath?:""
+//    }
+//
+//    override fun getCrewMovieBackdropPath(position: Int): String {
+//        return personCredits.crew[position].backdropPath?:""
+//    }
+
     override fun getCastMovieId(position: Int): Int {
-        return personCredits.cast.get(position).id
+        return cast.get(position).id
     }
 
     override fun getCastMovieName(position: Int): String {
-        return personCredits.cast.get(position).title
+        return cast.get(position).title
     }
 
     override fun getCharacter(position: Int): String {
-        return personCredits.cast.get(position).character
+        return cast.get(position).character
     }
 
     override fun getCastMoviePosterPath(position: Int): String {
-        return personCredits.cast.get(position).posterPath?:""
+        return cast.get(position).posterPath?:""
     }
 
     override fun getCrewMovieId(position: Int): Int {
-        return personCredits.crew.get(position).id
+        return crew.get(position).id
     }
 
     override fun getCrewMovieName(position: Int): String {
-        return personCredits.crew.get(position).title
+        return crew.get(position).title
     }
 
     override fun getJob(position: Int): String {
-        return personCredits.crew.get(position).job
+        return TextUtil.convertToCommaSeparatedString("",
+                crew.get(position).jobs,{it->it})
     }
 
     override fun getCrewMoviePosterPath(position: Int): String {
-        return personCredits.crew.get(position).posterPath?:""
+        return crew.get(position).posterPath?:""
     }
 
     override fun getCastMovieBackdropPath(position: Int): String {
-        return personCredits.cast[position].backdropPath?:""
+        return cast[position].backdropPath?:""
     }
 
     override fun getCrewMovieBackdropPath(position: Int): String {
-        return personCredits.crew[position].backdropPath?:""
+        return crew[position].backdropPath?:""
     }
 }
+
+data class CrewMultipleJobs(val id:Int,
+                            val order:Int,
+                            val title:String,
+                            var jobs:ArrayList<String>,
+                            val posterPath:String,
+                            val backdropPath:String)
